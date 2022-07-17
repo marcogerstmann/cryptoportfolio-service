bb0987ec12fdf8d0c8db8a98f193c48923c9cf22
Marco Gerstmann <m.gerstmann@gmail.com>
2022-07-17 16:48:33 +0200

    CP-22:feat:add CrossOrigin annotation

--- src/main/java/com/marcogerstmann/cryptoportfolio/service/controller/PortfolioStatisticsController.java
+++ src/main/java/com/marcogerstmann/cryptoportfolio/service/controller/PortfolioStatisticsController.java
@@ -5,6 +5,7 @@ import com.marcogerstmann.cryptoportfolio.service.dto.OverallPortfolioStatistics
 import com.marcogerstmann.cryptoportfolio.service.service.PortfolioStatisticsService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/1.0/portfolio/statistics")
 @AllArgsConstructor
+@CrossOrigin
 public class PortfolioStatisticsController {
 
     private final PortfolioStatisticsService portfolioStatisticsService;
--- src/main/java/com/marcogerstmann/cryptoportfolio/service/service/impl/CoinMarketValueServiceImpl.java
+++ src/main/java/com/marcogerstmann/cryptoportfolio/service/service/impl/CoinMarketValueServiceImpl.java
@@ -4,7 +4,6 @@ import com.marcogerstmann.cryptoportfolio.service.api.facade.ExternalDataProvide
 import com.marcogerstmann.cryptoportfolio.service.api.model.CoinQuote;
 import com.marcogerstmann.cryptoportfolio.service.service.CoinMarketValueService;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
